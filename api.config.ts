219f4e831f2acb3b9d63c31dc33f1198878c7e2f
gokulsam07 <gokuls2381@gmail.com>
2024-06-23 10:49:09 +0530

    Project skeleton

---
+++ api.config.ts
@@ -0,0 +1,29 @@
+import { PlaywrightTestConfig, chromium,firefox,webkit } from "@playwright/test";
+
+const config = {
+    timeout :60000,
+    testDir:'./tests/api',
+    fullyParallel: true,
+    reporter: 'html',
+    workers:5,
+    use:{
+        headless:false,
+        viewport:{width :1280, height :720},
+        actionTimeout :15000, 
+        screenshot:"on",
+        ignoreHTTPErrors:true, 
+    },
+    projects: [
+        {
+            name: 'Chromium',
+            use: { browserName: 'chromium' }
+        }, {
+            name: 'Firefox',
+            use: { browserName: 'firefox' }
+        }, {
+            name: 'WebKit',
+            use: { browserName: 'webkit' }
+        }]
+}
+
+export default config;
---
+++ ui.config.ts
@@ -0,0 +1,30 @@
+import { PlaywrightTestConfig, chromium,firefox,webkit } from "@playwright/test";
+
+const config = {
+    timeout :60000,
+    testDir:'/tests/ui',
+    fullyParallel: true,
+    reporter: 'html',
+    workers:5,
+    use:{
+        headless:false,
+        viewport:{width :1280, height :720},
+        actionTimeout :15000, 
+        video:"off",
+        screenshot: "only-on-failure",
+        ignoreHTTPErrors:true, 
+    },
+    projects: [
+        {
+            name: 'Chromium',
+            use: { browserName: 'chromium' }
+        }, {
+            name: 'Firefox',
+            use: { browserName: 'firefox' }
+        }, {
+            name: 'WebKit',
+            use: { browserName: 'webkit' }
+        }]
+}
+
+export default config;
